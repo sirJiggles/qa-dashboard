30e158b21b58649b3988279ae9b374604cee53b6
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2019-03-28 22:21:50 +0100

    add a nice little toggle for the side bar

---
+++ client/app/components/page-container.ts
@@ -0,0 +1,8 @@
+import Component from '@ember/component';
+
+export default class PageContainer extends Component.extend({
+  // anything which *must* be merged to prototype here
+}) {
+  // by default the side bar is open
+  sideBarOpen = true;
+}
