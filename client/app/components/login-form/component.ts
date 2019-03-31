eef7db066e68c9beada09dd8bc370cb2a0dfd52e
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2019-03-31 15:26:02 +0200

    adding all of the things to try get the login gtask to work, feels like there are issues however will have to speak with jan :D

---
+++ client/app/components/login-form/component.ts
@@ -0,0 +1,19 @@
+import Component from '@glimmer/component';
+import { computed } from '@ember/object';
+import { task } from 'ember-concurrency';
+
+export default class LoginForm extends Component {
+  args: {
+    login: task;
+  } = this.args;
+
+  // component state
+  username = '';
+  password = '';
+
+  // tracked seemed to not work here :/
+  @computed('username', 'password')
+  get invalid(): boolean {
+    return this.username === '' || this.password === '';
+  }
+}
--- client/app/routes/login.ts
+++ client/app/routes/login.ts
@@ -1,6 +1,20 @@
 import Route from '@ember/routing/route';
 import titleSetter from 'client/mixins/title-setter';
+import { task, timeout } from 'ember-concurrency';
 
 export default class Login extends Route.extend(titleSetter) {
   title = 'login';
+
+  login = task(function*() {
+    console.log('this is called');
+
+    timeout(5000);
+    yield true;
+  });
+
+  model() {
+    return {
+      loginTask: this.login
+    };
+  }
 }
