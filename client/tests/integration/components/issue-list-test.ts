07f5bcde54cd4d4569070ae561eba6dd65d59fa2
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2019-03-31 13:54:25 +0200

    fix some tests

--- client/tests/integration/components/issue-list-test.ts
+++ client/tests/integration/components/issue-list-test.ts
@@ -8,11 +8,10 @@ module('Integration | Component | issue-list', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it shows a list of issues and a title', async function(assert) {
-    this.set('title', 'some title');
     this.set('issues', ['one', 'two']);
 
-    await render(hbs`{{issue-list title=title issues=issues}}`);
+    await render(hbs`{{issue-list issues=issues}}`);
 
-    assert.dom('[data-test-issues-title]').hasText('some title');
+    assert.dom('[data-test-issues-title]').hasText('WIP');
   });
 });
